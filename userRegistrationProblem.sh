97fcfe17ec16a90f35a426e05d347480350014c1
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-29 20:52:04 +0530

    Added:check first name is valid or not

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -2,3 +2,18 @@
 shopt -s extglob
 
 echo "****user registration **** "
+
+pattern="^[A-Z][a-z]{2,}$"
+read -p "enter a first name:" firstName
+function checkPattern()
+{
+	local userName=$1 Pattern=$2
+	if [[ $userName =~ $Pattern ]] 
+	then
+		 echo "valid Name" 
+	else
+		 echo "Invalid Name"
+	fi
+}
+checkPattern $firstName $pattern
+
