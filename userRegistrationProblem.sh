6d788cb6f9583660467ab277225a891d5a9ed976
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-29 21:06:52 +0530

    Added:check email address is valid or not

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -3,6 +3,7 @@ shopt -s extglob
 
 echo "****user registration **** "
 
+#pattern checking
 pattern="^[A-Z][a-z]{2,}$"
 read -p "enter a first name:" firstName
 read -p "enter a last name:" lastName
@@ -18,3 +19,20 @@ function checkPattern()
 }
 checkPattern $firstName $pattern
 checkPattern $lastName $pattern
+
+#check email pattern
+emailPattern="^[a-zA-Z]{1,}([.]?[a-zA-Z]{1,})?[@]{1}[a-zA-Z]{1,}[.]{1}[a-z]{2}([.]?[a-z]{2})?$"
+read -p "enter a email adress:" emailid
+
+function checkPattern()
+{
+        local userName=$1 Pattern=$2
+        if [[ $userName =~ $Pattern ]]
+        then
+                 echo "valid email"
+        else
+                 echo "Invalid email"
+        fi
+}
+checkPattern $emailid $emailPattern
+
